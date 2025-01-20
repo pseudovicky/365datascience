a05f78a40dd7665b7fc4fed95ea9250d9336cbaa
Vicky Kumar <138932158+pseudovicky@users.noreply.github.com>
2025-01-20 23:32:09 +0530

    using time module in python

---
+++ TimeModule.py
@@ -0,0 +1,12 @@
+import time
+id = input("Enter the insta id : ")
+print("dictionary attack starting....")
+print("Waiting..")
+for i in range(30):
+    print(".")
+    time.sleep(1)
+    for i in range(10):
+        print("..",end="")
+print()
+print("Hacking performed Successfully...")
+print("Password is @Mr_robot")
